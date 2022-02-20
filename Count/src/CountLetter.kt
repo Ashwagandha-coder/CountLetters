eec24f554bce02a947bc49816e6a8fbb8f53bf8a
Ashwagandha-coder <max.cacz@yandex.ru>
2022-02-20 12:28:42 +0500

    new

---
+++ Count/src/CountLetter.kt
@@ -0,0 +1,14 @@
+/**
+ Дана сторока пример: AAABBCCCDDXYUUUUR
+нужно из данной стороки сделать такую строку пример: A3B2C3D2XYU4R
+ */
+
+fun main() {
+
+
+}
+
+fun countLetters(str: String): String {
+
+
+}
